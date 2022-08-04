0f87ab4524779fa0ac9728a826a7d4921a0e89fd
joohongpark <daleaderprofq@gmail.com>
2022-08-04 15:27:53 +0900

    feat: #7 add HTTP API & mock data

--- src/tag-log-v1/tag-log.controller.ts
+++ src/tag-log-v1/tag-log.controller.ts
@@ -10,6 +10,9 @@ import {
 import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CheckLogin } from 'src/auth/guard/check-login.guard';
 import { User } from 'src/auth/user.decorator';
+import InOut from 'src/enums/inout.enum';
+import { UserAccumulationType } from './dto/user-accumulation.type';
+import { UserInfoType } from './dto/user-Info.type';
 import { UserInOutLogsType } from './dto/UserInOutLogs.type';
 import { TagLogService } from './tag-log.service';
 
@@ -40,6 +43,10 @@ export class TagLogController {
   })
   @ApiResponse({ status: 400, description: '쿼리 타입 에러' })
   @ApiResponse({ status: 401, description: '접근 권한 없음' })
+  @ApiResponse({
+    status: 500,
+    description: '서버 내부 에러 (백앤드 관리자 문의 필요)',
+  })
   @ApiQuery({
     name: 'year',
     description: '년도',
@@ -97,6 +104,10 @@ export class TagLogController {
   })
   @ApiResponse({ status: 400, description: '쿼리 타입 에러' })
   @ApiResponse({ status: 401, description: '접근 권한 없음' })
+  @ApiResponse({
+    status: 500,
+    description: '서버 내부 에러 (백앤드 관리자 문의 필요)',
+  })
   @ApiQuery({
     name: 'year',
     description: '년도',
@@ -129,5 +140,68 @@ export class TagLogController {
       profileImage: user.image_url,
       inOutLogs,
     };
+  }
+
+  /**
+   * 로그인한 유저가 메인 화면에 접속할 때 가져올 정보를 반환합니다.
+   */
+  @ApiOperation({
+    summary: '사용자 접속 시 보여줄 메인 정보',
+    description:
+      '로그인한 유저가 메인 화면에 접속할 때 가져올 정보를 조회합니다.',
+  })
+  @ApiResponse({
+    status: 200,
+    type: UserInfoType,
+    description: '조회 성공',
+  })
+  @ApiResponse({ status: 401, description: '접근 권한 없음' })
+  @ApiResponse({
+    status: 500,
+    description: '서버 내부 에러 (백앤드 관리자 문의 필요)',
+  })
+  @Get('maininfo')
+  @UseGuards(CheckLogin)
+  async getMainInfo(
+    @User(new ValidationPipe({ validateCustomDecorators: true })) user: any,
+  ): Promise<UserInfoType> {
+    this.logger.debug(`call getMainInfo request by ${user.login}`);
+    const result: UserInfoType = {
+      login: user.login,
+      profileImage: user.image_url,
+      isAdmin: user.isAdmin,
+      inoutState: InOut.IN,
+    };
+    return result;
+  }
+
+  /**
+   * 로그인한 유저의 일별/월별 누적 체류시간을 반환합니다.
+   */
+  @ApiOperation({
+    summary: '로그인한 유저의 일별/월별 누적 체류시간',
+    description: '로그인한 유저의 일별/월별 누적 체류시간을 조회합니다.',
+  })
+  @ApiResponse({
+    status: 200,
+    type: UserAccumulationType,
+    description: '조회 성공',
+  })
+  @ApiResponse({ status: 401, description: '접근 권한 없음' })
+  @ApiResponse({
+    status: 500,
+    description: '서버 내부 에러 (백앤드 관리자 문의 필요)',
+  })
+  @Get('accumulationTimes')
+  @UseGuards(CheckLogin)
+  async getAccumulationTimes(
+    @User(new ValidationPipe({ validateCustomDecorators: true })) user: any,
+  ): Promise<UserAccumulationType> {
+    this.logger.debug(`call getAccumulationTimes request by ${user.login}`);
+    const result: UserAccumulationType = {
+      todayAccumationTime: 3600,
+      monthAccumationTime: 12800,
+    };
+    return result;
   }
 }
