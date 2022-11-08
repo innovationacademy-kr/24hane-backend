ca0e967ca298e25e5e52b746186dd074333afb0c
joohongpark <daleaderprofq@gmail.com>
2022-11-08 17:41:47 +0900

    fix: #76

--- src/user/repository/interface/user-info-repository.interface.ts
+++ src/user/repository/interface/user-info-repository.interface.ts
@@ -1,19 +1,18 @@
+import { CardDto } from 'src/user/dto/card.dto';
 import { IdLoginDto } from 'src/user/dto/id-login.dto';
 
 export interface IUserInfoRepository {
   /**
    * 사용자 ID로 사용자에 대한 카드 ID 목록을 반환합니다.
+   * 발급일과 만료일 내의 카드 발급 기록을 가지고 옵니다.
+   * begin <= 카드의_만료_시간 && end >= 카드의_발급_시간 관계를 이용합니다.
    *
-   * @param userId 사용자 아이디
-   * @param vaildEnd 최소 만료기간 (optional)
-   * @param vaildStart 카드 발급한 기간보다 작은 기간 (optional)
+   * @param id 사용자 아이디
+   * @param begin 발급일 (optional)
+   * @param end 만료일 (optional)
    * @returns 카드 ID 목록 (배열)
    */
-  findCardIds(
-    userId: number,
-    vaildEnd?: Date,
-    vaildStart?: Date,
-  ): Promise<string[]>;
+  findCardsByUserId(id: number, begin?: Date, end?: Date): Promise<CardDto[]>;
 
   /**
    * 사용자 로그인 ID로 사용자의 ID를 반환합니다. 존재하지 않는 로그인 ID면 -1을 리턴합니다
