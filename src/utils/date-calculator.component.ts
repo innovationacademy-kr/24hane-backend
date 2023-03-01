3185151fd43b61f665a026a86ee61da30c466cb9
niamu01 <yeju@student.42seoul.kr>
2023-03-01 14:52:09 +0900

    fix: 한주의 기준을 월~일로 변경
    - #117

--- src/utils/date-calculator.component.ts
+++ src/utils/date-calculator.component.ts
@@ -50,18 +50,18 @@ export class DateCalculator {
   }
 
   /**
-   * 인자로 주어진 일에 대해 그 주의 첫 날짜(일요일)의 가장 빠른 시간을 반환합니다.
+   * 인자로 주어진 일에 대해 그 주의 첫 날짜(월요일)의 가장 빠른 시간을 반환합니다.
    * NOTE: 서버의 로컬 시간에 맞게 동작함에 유의해야 함.
    */
   getStartOfWeek(date: Date): Date {
-    const dayOfWeek = date.getDay();
-    const diff = dayOfWeek === 0 ? 0 : dayOfWeek - 1;
+    const dayOfWeek = date.getDay() - 1;
+    const diff = dayOfWeek === -1 ? 6 : dayOfWeek;
     const rtn = new Date(date.getFullYear(), date.getMonth(), date.getDate() - diff);
     return rtn;
   }
 
   /**
-   * 인자로 주어진 일에 대해 그 주의 마지막 날짜(토요일)와 가장 늦은 시간을 반환합니다.
+   * 인자로 주어진 일에 대해 그 주의 마지막 날짜(일요일)와 가장 늦은 시간을 반환합니다.
    * NOTE: 서버의 로컬 시간에 맞게 동작함에 유의해야 함.
    */
   getEndOfWeek(date: Date): Date {
@@ -69,9 +69,6 @@ export class DateCalculator {
     const rtn = new Date(startOfWeek);
     rtn.setDate(startOfWeek.getDate() + 7);
     rtn.setTime(rtn.getTime() - 1);
-    //const rtn = new Date(startOfWeek.getTime() + 7 * 24 * 60 * 60 * 1000);
-    //const rtn = new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate() + 7);
-    //rtn.setDate(rtn.getTime() - 1);
     return rtn;
   }
 
