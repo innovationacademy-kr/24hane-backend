6acbfd7bd17dd91329717a8c78891ff5f40d573e
niamu01 <niamu01@naver.com>
2023-12-19 01:31:05 +0900

    add: 미리 배포를 위해 v2 함수 복구

--- src/tag-log/dto/user-Info.type.ts
+++ src/tag-log/dto/user-Info.type.ts
@@ -3,6 +3,55 @@ import InOut from 'src/enums/inout.enum';
 import { InfoMessageDto } from './info-message.dto';
 
 /**
+ * @version 4
+ * For removal in 5.0
+ */
+export class UserInfoTypeV4 {
+  @ApiProperty({
+    description: '42 로그인 ID',
+    example: 'joopark',
+  })
+  login: string;
+
+  @ApiProperty({
+    description: '인트라 이미지 URI',
+    example: 'https://cdn.intra.42.fr/users/joopark.jpg',
+  })
+  profileImage: string;
+
+  @ApiProperty({
+    description: '42 관리자 여부',
+    example: false,
+  })
+  isAdmin: boolean;
+
+  @ApiPropertyOptional({
+    description: '개포 체류인원 (Optional)',
+    example: 42,
+  })
+  gaepo?: number;
+
+  @ApiPropertyOptional({
+    description: '서초 체류인원 (Optional)',
+    example: 42,
+  })
+  seocho?: number;
+
+  @ApiProperty({
+    description: '본인의 클러스터 체류 여부',
+    enum: ['IN', 'OUT'],
+    example: 'OUT',
+  })
+  inoutState: InOut;
+
+  @ApiProperty({
+    description: '가장 최근에 태깅한 기록 (태깅한 기록이 없다면 null)',
+    example: null,
+  })
+  tagAt: Date;
+}
+
+/**
  * @version 4: login, profileImage, isAdmin, gaepo, seocho, inoutState, tagAt
  * @version 5: login, profileImage, isAdmin, gaepo, infoMessages, inoutState, tagAt
  */
--- src/tag-log/tag-log.controller.ts
+++ src/tag-log/tag-log.controller.ts
@@ -1,6 +1,8 @@
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import {
   Controller,
   Get,
+  Inject,
   Logger,
   ParseIntPipe,
   Query,
@@ -13,10 +15,14 @@ import {
   ApiResponse,
   ApiTags,
 } from '@nestjs/swagger';
+import { Cache } from 'cache-manager';
 import { UserSessionDto } from 'src/auth/dto/user.session.dto';
 import { UserAuthGuard } from 'src/auth/guard/user-auth.guard';
 import { User } from 'src/auth/user.decorator';
+import { CadetPerClusterDto } from 'src/statistics/dto/cadet-per-cluster.dto';
+import { StatisticsService } from 'src/statistics/statictics.service';
 import { UserInOutLogsType } from './dto/UserInOutLogs.type';
+import { UserInfoTypeV4 } from './dto/user-Info.type';
 import { UserAccumulationTypeV2 } from './dto/user-accumulation.type.v2';
 import { TagLogService } from './tag-log.service';
 
@@ -30,7 +36,11 @@ import { TagLogService } from './tag-log.service';
 export class TagLogController {
   private logger = new Logger(TagLogController.name);
 
-  constructor(private tagLogService: TagLogService) {}
+  constructor(
+    private tagLogService: TagLogService,
+    private statisticsService: StatisticsService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
 
   /**
    * 특정 일에 대한 모든 태그로그를 조회합니다.
@@ -206,6 +216,48 @@ export class TagLogController {
       sixMonthAccumulationTime,
     };
 
+    return result;
+  }
+
+  /**
+   * 로그인한 유저가 메인 화면에 접속할 때 가져올 정보를 반환합니다.
+   */
+  @ApiOperation({
+    summary: '사용자 접속 시 보여줄 메인 정보',
+    description:
+      '로그인한 유저가 메인 화면에 접속할 때 가져올 정보를 조회합니다.',
+  })
+  @ApiResponse({
+    status: 200,
+    type: UserInfoTypeV4,
+    description: '조회 성공',
+  })
+  @ApiResponse({ status: 401, description: '접근 권한 없음' })
+  @ApiResponse({
+    status: 500,
+    description: '서버 내부 에러 (백앤드 관리자 문의 필요)',
+  })
+  @Get('maininfo')
+  async getMainInfo(@User() user: UserSessionDto): Promise<UserInfoTypeV4> {
+    this.logger.debug(`@getMainInfo) by ${user.login}`);
+    const inoutState = await this.tagLogService.checkClusterById(user.user_id);
+    // FIXME: 추후에 캐시 관련 리팩터링 필요
+    let cadetPerCluster: undefined | CadetPerClusterDto[] =
+      await this.cacheManager.get('getCadetPerClusterV4');
+    if (cadetPerCluster === undefined) {
+      cadetPerCluster = await this.statisticsService.getCadetPerCluster(2);
+      await this.cacheManager.set('getCadetPerClusterV4', cadetPerCluster, 60);
+    }
+    const gaepo = +cadetPerCluster.find((v) => v.cluster === 'GAEPO')?.cadet;
+    const result: UserInfoTypeV4 = {
+      login: user.login,
+      profileImage: user.image_url,
+      isAdmin: user.is_staff,
+      inoutState: inoutState.inout,
+      tagAt: inoutState.log,
+      gaepo: gaepo ? gaepo : 0,
+      seocho: 0,
+    };
     return result;
   }
 }
