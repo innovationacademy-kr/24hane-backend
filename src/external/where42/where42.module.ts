d5b332c0cd81cb2778c2b3200925392a7d598fc6
niamu01 <niamu01@naver.com>
2024-07-10 00:11:52 +0900

    module import 오류 수정

--- src/external/where42/where42.module.ts
+++ src/external/where42/where42.module.ts
@@ -4,6 +4,7 @@ import { DeviceInfo } from 'src/entities/device-info.entity';
 import { TagLog } from 'src/entities/tag-log.entity';
 import { TagLogRepository } from 'src/tag-log/repository/mysql/tag-log.repository';
 import { UserModule } from 'src/user/user.module';
+import { DateCalculatorModule } from 'src/utils/data-calculator/data-calculator.module';
 import { DeviceInfoRepository } from './repository/mysql/device-info.repository';
 import { Where42Controller } from './where42.controller';
 import { Where42Service } from './where42.service';
@@ -22,7 +23,11 @@ const deviceInfoRepo = {
  * 24Hane 기능 외 다른 서비스에서 사용하는 외부 API 모듈
  */
 @Module({
-  imports: [UserModule, TypeOrmModule.forFeature([DeviceInfo, TagLog])],
+  imports: [
+    UserModule,
+    TypeOrmModule.forFeature([DeviceInfo, TagLog]),
+    DateCalculatorModule,
+  ],
   controllers: [Where42Controller],
   providers: [tagLogRepo, deviceInfoRepo, Where42Service],
 })
